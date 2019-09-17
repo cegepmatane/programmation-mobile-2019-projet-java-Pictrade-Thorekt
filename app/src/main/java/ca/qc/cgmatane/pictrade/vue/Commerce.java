017adea441d338fdd07496efe31acec7ccbd3f76
Thorekt <35566455+Thorekt@users.noreply.github.com>
2019-09-17 10:52:46 -0400

    ajout de la vue d'affichage d'un commerce

---
+++ app/src/main/java/ca/qc/cgmatane/pictrade/vue/Commerce.java
@@ -0,0 +1,16 @@
+package ca.qc.cgmatane.pictrade.vue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.qc.cgmatane.pictrade.R;
+
+public class Commerce extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_commerce);
+    }
+}
