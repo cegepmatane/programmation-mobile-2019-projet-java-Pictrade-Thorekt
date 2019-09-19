7b96ba3246a488cdfdc68616543357d6ce1b74e0
Adrian <adrian.paul.carrieres@gmail.com>
2019-09-19 17:24:57 -0400

    Ajout fonction listerCommerce() [CommerceDAO]
    Finition de la classe Commercer au passage pour permettre la fonction listerCommerce() de fonctionner

--- app/src/main/java/ca/qc/cgmatane/pictrade/donnee/CommerceDAO.java
+++ app/src/main/java/ca/qc/cgmatane/pictrade/donnee/CommerceDAO.java
@@ -1,5 +1,8 @@
 package ca.qc.cgmatane.pictrade.donnee;
 
+import android.database.Cursor;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import ca.qc.cgmatane.pictrade.modele.Commerce;
@@ -9,4 +12,46 @@ public class CommerceDAO {
     protected BaseDeDonnees accesseurBaseDeDonnees;
 
     protected List<Commerce> listeCommerces;
+
+    public static CommerceDAO getInstance() {
+        if (instance == null) {
+            instance = new CommerceDAO();
+        }
+        return instance;
+    }
+
+    public CommerceDAO(){
+        accesseurBaseDeDonnees = BaseDeDonnees.getInstance();
+        listeCommerces = new ArrayList<>();
+    }
+
+    public List<Commerce> ListerCommerce(){
+        String LISTER_COMMERCE = "SELECT id, nom, longitude, latitude, horaire, adresse, contact FROM commerce";
+        Cursor curseur = accesseurBaseDeDonnees.getReadableDatabase().rawQuery(LISTER_COMMERCE,
+                null);
+        this.listeCommerces.clear();
+
+        int indexId = curseur.getColumnIndex("id");
+        int indexNom = curseur.getColumnIndex("nom");
+        int indexLongitude = curseur.getColumnIndex("longitude");
+        int indexLatitude = curseur.getColumnIndex("latitude");
+        int indexHoraire = curseur.getColumnIndex("horaire");
+        int indexAdresse = curseur.getColumnIndex("adresse");
+        int indexContact = curseur.getColumnIndex("contact");
+
+        for(curseur.moveToFirst(); !curseur.isAfterLast(); curseur.moveToNext()){
+            int id = curseur.getInt(indexId);
+            String nom = curseur.getString(indexNom);
+            float longitude = curseur.getFloat(indexLongitude);
+            float latitude = curseur.getFloat(indexLatitude);
+            String horaire = curseur.getString(indexHoraire);
+            String adresse = curseur.getString(indexAdresse);
+            String contact = curseur.getString(indexContact);
+
+            listeCommerces.add(new Commerce(id, nom, longitude, latitude, horaire, adresse, contact));
+        }
+
+        return listeCommerces;
+    }
+
 }
--- app/src/main/java/ca/qc/cgmatane/pictrade/modele/Commerce.java
+++ app/src/main/java/ca/qc/cgmatane/pictrade/modele/Commerce.java
@@ -1,4 +1,100 @@
 package ca.qc.cgmatane.pictrade.modele;
 
+import java.util.Objects;
+
 public class Commerce {
+    public int id;
+    public String nom;
+    public float longitude;
+    public float latitude;
+    public String horaire;
+    public String adresse;
+    public String contact;
+
+    public Commerce(int id, String nom, float longitude, float latitude, String horaire, String adresse, String contact) {
+        this.id = id;
+        this.nom = nom;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.horaire = horaire;
+        this.adresse = adresse;
+        this.contact = contact;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getHoraire() {
+        return horaire;
+    }
+
+    public void setHoraire(String horaire) {
+        this.horaire = horaire;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Commerce commerce = (Commerce) o;
+        return id == commerce.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Commerce{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", longitude=" + longitude +
+                ", latitude=" + latitude +
+                ", horaire='" + horaire + '\'' +
+                ", adresse='" + adresse + '\'' +
+                ", contact='" + contact + '\'' +
+                '}';
+    }
 }
