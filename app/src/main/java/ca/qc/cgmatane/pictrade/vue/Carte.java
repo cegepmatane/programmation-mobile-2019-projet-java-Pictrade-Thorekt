8c1ee0af22af910a9611462093f65f9f0569cbe6
Thorekt <35566455+Thorekt@users.noreply.github.com>
2019-09-24 11:14:26 -0400

    modifcation du mode la carte

--- app/src/main/java/ca/qc/cgmatane/pictrade/vue/Carte.java
+++ app/src/main/java/ca/qc/cgmatane/pictrade/vue/Carte.java
@@ -39,7 +39,6 @@ public class Carte extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
