e8a9a231712ea0ae407561d4d9fc0d0bbc9605db
Thorekt <35566455+Thorekt@users.noreply.github.com>
2019-09-26 10:00:26 -0400

    modification du manifeste pour connection internet

--- app/src/main/java/ca/qc/cgmatane/pictrade/donnee/BaseDeDonneesServeur.java
+++ app/src/main/java/ca/qc/cgmatane/pictrade/donnee/BaseDeDonneesServeur.java
@@ -4,40 +4,16 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class BaseDeDonneesServeur extends SQLiteOpenHelper {
+public class BaseDeDonneesServeur {
 
     private static BaseDeDonneesServeur instance = null;
 
-    public static BaseDeDonneesServeur getInstance(Context contexte) {
-        instance = new BaseDeDonneesServeur(contexte);
-        return instance;
-    }
-
     public static BaseDeDonneesServeur getInstance() {
+        if (instance != null) {
+            instance = new BaseDeDonneesServeur();
+        }
         return instance;
     }
 
-    /*public BaseDeDonneesServeur(Context contexte) {
-
-    }
-
-    public BaseDeDonneesServeur(Context contexte, String name, SQLiteDatabase.CursorFactory factory, int version) {
-
-    }*/
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-
-    }
-
-    @Override
-    public void onOpen(SQLiteDatabase db) {
-
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int arg1, int arg2) {
-
-    }
 
 }
