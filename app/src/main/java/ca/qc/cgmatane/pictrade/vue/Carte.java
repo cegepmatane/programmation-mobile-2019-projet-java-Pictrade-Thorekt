9d109bf54a1f6e53718984acd07eed0462001006
Quentin <quentin.bernard24@orange.fr>
2019-09-26 17:42:44 -0400

    test

--- app/src/main/java/ca/qc/cgmatane/pictrade/vue/Carte.java
+++ app/src/main/java/ca/qc/cgmatane/pictrade/vue/Carte.java
@@ -40,8 +40,8 @@ public class Carte extends FragmentActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng matane = new LatLng(48.8445, -67.5306);
+        mMap.addMarker(new MarkerOptions().position(matane).title("Marker in matane"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(matane));
     }
 }
