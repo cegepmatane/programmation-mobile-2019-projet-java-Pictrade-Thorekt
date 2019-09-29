851b759150cb07aade02f3058b3080a549e0e33c
Thorekt <35566455+Thorekt@users.noreply.github.com>
2019-09-29 19:14:27 -0400

    interface controlleur

--- app/src/main/java/ca/qc/cgmatane/pictrade/controleur/controleur.java
+++ app/src/main/java/ca/qc/cgmatane/pictrade/controleur/controleur.java
@@ -1,4 +1,11 @@
 package ca.qc.cgmatane.pictrade.controleur;
 
+import android.content.Context;
+
 public interface controleur {
+    void onCreate(Context applicationContext);
+    void onPause();
+    void onResume();
+    void onDestroy();
+    void onActivityResult(int activite);
 }
