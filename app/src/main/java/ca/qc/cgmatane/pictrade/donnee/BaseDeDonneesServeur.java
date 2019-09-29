76446a4820429bc3c51a52d3586f68b3d2cfe002
Thorekt <35566455+Thorekt@users.noreply.github.com>
2019-09-29 12:20:53 -0400

    virification de la connexion + methode de recuperation de xml

--- app/src/main/java/ca/qc/cgmatane/pictrade/donnee/BaseDeDonneesServeur.java
+++ app/src/main/java/ca/qc/cgmatane/pictrade/donnee/BaseDeDonneesServeur.java
@@ -3,6 +3,13 @@ package ca.qc.cgmatane.pictrade.donnee;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class BaseDeDonneesServeur {
 
@@ -14,6 +21,30 @@ public class BaseDeDonneesServeur {
         }
         return instance;
     }
+
+
+
+    private static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    public InputStream recupererXML(String page,Context context){
+
+        isNetworkAvailable(context);
+
+        URL url = new URL("http://51.91.96.142/");
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            return (in);
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
 
 
 }
