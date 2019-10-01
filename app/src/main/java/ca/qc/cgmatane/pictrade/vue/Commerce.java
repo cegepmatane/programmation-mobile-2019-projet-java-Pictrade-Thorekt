41ac9d2ded084a5060b1688296b1ebad24f5aed8
Lucas1406 <lucas.portal@orange.fr>
2019-10-01 11:23:15 -0400

    Ajout informationCommerce

--- app/src/main/java/ca/qc/cgmatane/pictrade/vue/Commerce.java
+++ app/src/main/java/ca/qc/cgmatane/pictrade/vue/Commerce.java
@@ -3,11 +3,13 @@ package ca.qc.cgmatane.pictrade.vue;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import ca.qc.cgmatane.pictrade.R;
 import ca.qc.cgmatane.pictrade.donnee.CommerceDAO;
 
 public class Commerce extends AppCompatActivity implements VueCommerce {
+    protected CommerceDAO accesseurCommerce;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +18,9 @@ public class Commerce extends AppCompatActivity implements VueCommerce {
     }
 
     protected void informationCommerce() {
-        
+        accesseurCommerce = CommerceDAO.getInstance();
+
+        //vueListeCommerces = (ListView) findViewById(R.id.vue_detail_commerce)
     }
 }
 
