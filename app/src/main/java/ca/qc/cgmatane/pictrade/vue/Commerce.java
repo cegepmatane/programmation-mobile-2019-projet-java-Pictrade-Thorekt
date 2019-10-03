6b76bad36c8400a9505d33c3b0a36470c79c82a8
Lucas1406 <lucas.portal@orange.fr>
2019-10-03 16:35:34 -0400

    Tentative remplissage

--- app/src/main/java/ca/qc/cgmatane/pictrade/vue/Commerce.java
+++ app/src/main/java/ca/qc/cgmatane/pictrade/vue/Commerce.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import ca.qc.cgmatane.pictrade.R;
 import ca.qc.cgmatane.pictrade.donnee.CommerceDAO;
@@ -17,10 +18,19 @@ public class Commerce extends AppCompatActivity implements VueCommerce {
         setContentView(R.layout.vue_commerce);
     }
 
-    protected void informationCommerce() {
+    protected void informationCommerces() {
         accesseurCommerce = CommerceDAO.getInstance();
 
-        //vueListeCommerces = (ListView) findViewById(R.id.vue_detail_commerce)
+       //vueCommerces = (ListView) findViewById(R.id.vue_liste_commerce);
+
+        //listeDetailPourAdaptateur = accesseurCommerce.recupererListeCommercePourAdapteur();
+
+        //SimpleAdapter adapteurVueDetailCommerce = new SimpleAdapter(this,
+        //        listeDetailPourAdaptateur,
+        //        android.R.layout.two_line_list_item,
+        //        new int[]{android.R.id.text1, android.R.id.text2});
+
+        //adapteurVueDetailCommerce.setAdapter(adapteurVueDetailCommerce);
     }
 }
 
