f38e00483a0dab2aa53a3af4ef7623de8ca63357
Adrian <adrian.paul.carrieres@gmail.com>
2019-10-14 10:08:18 -0400

    ajout searchable activity

---
+++ app/src/main/java/ca/qc/cgmatane/pictrade/helper/SearchableActivity.java
@@ -0,0 +1,16 @@
+package ca.qc.cgmatane.pictrade.helper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.qc.cgmatane.pictrade.R;
+
+public class SearchableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchable);
+    }
+}
