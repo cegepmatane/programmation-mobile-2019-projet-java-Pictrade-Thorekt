2163990556906c2c91b44071f3fdf53e847a8bdf
Thorekt <35566455+Thorekt@users.noreply.github.com>
2019-10-16 23:33:54 -0400

    debut mise en place galerie

--- app/src/main/java/ca/qc/cgmatane/pictrade/vue/Galerie.java
+++ app/src/main/java/ca/qc/cgmatane/pictrade/vue/Galerie.java
@@ -1,16 +1,90 @@
 package ca.qc.cgmatane.pictrade.vue;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
 
 import ca.qc.cgmatane.pictrade.R;
+import ca.qc.cgmatane.pictrade.modele.Photo;
 
 public class Galerie extends AppCompatActivity implements VueGalerie{
+    private List<Photo> listePhoto;
+    private RecyclerView vueGalerieListePhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vue_galerie);
+
+
+
+        vueGalerieListePhoto = (RecyclerView) findViewById(R.id.vue_galerie_liste_photo);
+        vueGalerieListePhoto.setLayoutManager(new LinearLayoutManager(this));
+        vueGalerieListePhoto.setItemAnimator(new DefaultItemAnimator());
+
+        GalerieAdapteur galerieAdapteur = new GalerieAdapteur(R.layout.vue_ligne_galerie,listePhoto);
+        vueGalerieListePhoto.setAdapter(galerieAdapteur);
+    }
+
+
+    private class GalerieAdapteur extends RecyclerView.Adapter<GalerieAdapteur.ViewHolder>{
+        private int vueLigneGalerieLayout;
+        private List<Photo> listePhoto;
+
+        public GalerieAdapteur(int vueLigneGalerieLayout, List<Photo> listePhoto) {
+            this.vueLigneGalerieLayout = vueLigneGalerieLayout;
+            this.listePhoto = listePhoto;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View vue = LayoutInflater.from(parent.getContext()).inflate(vueLigneGalerieLayout, parent, false);
+            ViewHolder vueHolder = new ViewHolder(vue);
+            return vueHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            holder.vueLigneGaleriePhotoGauche.setImageBitmap(listePhoto.get(position).getImage());
+
+            holder.vueLigneGaleriePhotoMilieu.setImageBitmap(listePhoto.get(position+1).getImage());
+
+            holder.vueLigneGaleriePhotoDroite.setImageBitmap(listePhoto.get(position+2).getImage());
+        }
+
+        @Override
+        public int getItemCount() {
+            return listePhoto == null ? 0 : listePhoto.size();
+        }
+
+        // Static inner class to initialize the views of rows
+        private class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+            public ImageView vueLigneGaleriePhotoGauche;
+            public ImageView vueLigneGaleriePhotoMilieu;
+            public ImageView vueLigneGaleriePhotoDroite;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                vueLigneGaleriePhotoGauche =  (ImageView) itemView.findViewById(R.id.vue_ligne_galerie_photo_gauche);
+                vueLigneGaleriePhotoMilieu =  (ImageView) itemView.findViewById(R.id.vue_ligne_galerie_photo_milieu);
+                vueLigneGaleriePhotoDroite =  (ImageView) itemView.findViewById(R.id.vue_ligne_galerie_photo_droite);
+            }
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        }
     }
 }
