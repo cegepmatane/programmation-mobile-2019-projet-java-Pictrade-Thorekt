91a164650ee66e1fd14aaa6ed0b96d2ea0ebc3b3
Adrian <adrian.paul.carrieres@gmail.com>
2019-10-18 17:29:36 -0400

    favoriDAO ajouté

--- app/src/main/java/ca/qc/cgmatane/pictrade/donnee/FavoriDAO.java
+++ app/src/main/java/ca/qc/cgmatane/pictrade/donnee/FavoriDAO.java
@@ -1,116 +1,23 @@
 package ca.qc.cgmatane.pictrade.donnee;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteStatement;
-
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
-import ca.qc.cgmatane.pictrade.modele.Commerce;
+import ca.qc.cgmatane.pictrade.modele.Favori;
 
 public class FavoriDAO {
-
     private static FavoriDAO instance;
     private BaseDeDonneesClient accesseurBaseDeDonneesClient;
+    private ArrayList<Favori> listeFavori;
 
-    protected ArrayList<Commerce> listeFavori;
-
-    public static FavoriDAO getInstance() {
-        if (instance == null) {
+    public static FavoriDAO getInstance(){
+        if(instance == null){
             instance = new FavoriDAO();
         }
         return instance;
     }
 
-    private FavoriDAO(){
+    public FavoriDAO(){
         accesseurBaseDeDonneesClient = BaseDeDonneesClient.getInstance();
         listeFavori = new ArrayList<>();
-    }
-
-    public List<Commerce> listerFavori(){
-        String LISTER_FAVORIS = "SELECT * FROM favori";
-        Cursor curseur = accesseurBaseDeDonneesClient.getReadableDatabase().rawQuery(LISTER_FAVORIS,
-                null);
-        this.listeFavori.clear();
-
-        int indexId_commerce = curseur.getColumnIndex("id_commerce");
-        int indexIsFavori = curseur.getColumnIndex("isFavori");
-
-        for(curseur.moveToFirst(); !curseur.isAfterLast(); curseur.moveToNext()){
-            int id_commerce = curseur.getInt(indexId_commerce);
-            boolean isFavori = (curseur.getInt(indexIsFavori) == 1);
-            listeFavori.add(new Commerce(id_commerce));
-        }
-
-        return listeFavori;
-    }
-
-    public List<HashMap<String, String>> recupererListeFavoriPourAdapteur(){
-        List<HashMap<String, String>> listeFavoriPourAdapteur = new ArrayList<>();
-
-        listerFavori();
-
-        for(Commerce fav : listeFavori){
-            listeFavoriPourAdapteur.add(fav.obtenirCommercePourAdapteur());
-        }
-        return listeFavoriPourAdapteur;
-    }
-
-    public void ajouterFavori(Commerce favori){
-        listerFavori();
-        if(chercherFavoriParIdCommerce(favori.getId()) != null) {
-            SQLiteDatabase db = accesseurBaseDeDonneesClient.getWritableDatabase();
-            SQLiteStatement query = db.compileStatement("INSERT INTO favori(id_favori, id_commerce, isFavori) VALUES(null,?,?)");
-            query.bindString(1, "" + favori.getId());
-            query.bindString(2, "" + 1);
-            query.execute();
-            System.out.println("nique ta race");
-        }
-        else{
-            SQLiteDatabase db = accesseurBaseDeDonneesClient.getWritableDatabase();
-            SQLiteStatement query = db.compileStatement("UPDATE favori SET isFavori = ? where id_commerce = ?");
-            query.bindString(1, String.valueOf(1));
-            query.bindString(2, String.valueOf(favori.getId()));
-
-            query.execute();
-            System.out.println("nique ta race bis");
-        }
-        System.out.println("nique ta race ter");
-
-    }
-
-    public Commerce chercherFavoriParIdFavori(int id_favori){
-        listerFavori();
-        for(Commerce commerceRecherche : this.listeFavori){
-            if(commerceRecherche.getId() == id_favori) return commerceRecherche;
-        }
-        return null;
-    }
-
-    public Commerce chercherFavoriParIdCommerce(int id_commerce){
-        listerFavori();
-        for(Commerce commerceRecherche : this.listerFavori()){
-            if(commerceRecherche.getId() == id_commerce) return commerceRecherche;
-        }
-        return null;
-    }
-
-    public void retirerFavori(Commerce fav){
-        SQLiteDatabase db = accesseurBaseDeDonneesClient.getWritableDatabase();
-        SQLiteStatement query = db.compileStatement("UPDATE favori SET isFavori = ? where id_commerce = ?");
-        query.bindString(1, String.valueOf(0));
-        query.bindString(2, String.valueOf(fav.getId()));
-
-        query.execute();
-    }
-
-    public ArrayList<Commerce> matchFavori(ArrayList<Commerce> listeCommerce){
-        listerFavori();
-
-        listeCommerce.retainAll(listeFavori);
-
-        return listeCommerce;
     }
 }
